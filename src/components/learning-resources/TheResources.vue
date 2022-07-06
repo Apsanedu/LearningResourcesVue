35fcee95c3347f6f40b5f8f659c58afe73edf854
Apsanedu <abishwokarma@student.cccs.edu>
2022-07-05 21:42:32 -0700

    Model+View created

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,31 @@
+<template>
+<base-card>
+  <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+  <base-button @click="setSelectedTab('add-resources')">Add Resources</base-button>
+</base-card>
+</template>
+
+<script>
+import StoredResources from "@/components/learning-resources/StoredResources";
+import AddResources from "@/components/learning-resources/AddResources";
+
+export default {
+  components: [ 'StoredResources', 'AddResources'],
+  name: "TheResources",
+  data() {
+    return {
+      selectedTab: 'stored-resources'
+    }
+  },
+  methods: {
+    setSelectedTab (tab) {
+      this.selectedTab = tab;
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
